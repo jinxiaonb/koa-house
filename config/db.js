60fa156e197c41cc29559fefd6c867fab8cdee73
jinxiaonb <315232021@163.com>
2023-03-16 23:37:45 +0800

    服务端目录构建

---
+++ config/db.js
@@ -0,0 +1,21 @@
+
+/**
+ * 数据库服务器地址，账号，密码
+ * 
+*/
+const db = {
+    /**
+     * 
+    */
+    mysql_local: {
+
+    },
+    /**
+     * 
+    */
+    mysql_centos: {
+
+    }
+}
+
+module.exports = db;
